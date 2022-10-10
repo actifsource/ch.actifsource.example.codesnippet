622fe732ae8763e015ca63cbeb8b061046e3a0e5
ruti <grischa.rutishauser@actifsource.com>
2022-10-10 08:43:36 +0200

    add auto complete feature

--- code/TestSystem.c
+++ code/TestSystem.c
@@ -2,24 +2,28 @@
 /**
  * C Example
  */ 
-// Test codesnippet
+// Test codesnippet function variable
 if(Function_1(Var_2,1)){
     Var_1 = 77; 
 }else{
     Var_1 = 66;
 }
+// Test codesnippet cast
+Var_1 = (int)Var_2;
 
 
 /**
  * ST Example
  */ 
-// Test codesnippet
+// Test codesnippet function variable
 IF (Function_1(Var_2,1)) THEN 
     Var_1 := 77; 
 ELSE 
     Var_1 := 66;
 END_IF;
+// Test codesnippet cast
+Var_1 := #warning "Cast is not supported!";
 
 
 
-/* Actifsource ID=[a12233b6-98e4-11ea-8ef1-cb1cd2be9da3,49da7b0b-98e1-11ea-8ef1-cb1cd2be9da3,R+fISB1NcCdmYtMv1ShMT/QJDUU=] */
+/* Actifsource ID=[a12233b6-98e4-11ea-8ef1-cb1cd2be9da3,49da7b0b-98e1-11ea-8ef1-cb1cd2be9da3,qya43j07e6E9AMtBULwPXvs5HkI=] */
