a65dc817de58c1f307e73c11445aacfe329b74a4
ruti <grischa.rutishauser@actifsource.com>
2025-08-07 16:22:38 +0200

    Udate to Version 11.7.0

--- code/TestSystem.c
+++ code/TestSystem.c
@@ -1,3 +1,4 @@
+// CMinus
 
 /**
  * C Example
@@ -8,11 +9,12 @@ if(Function_1_code(Var_2_code,1)){
 }else{
     Var_1_code = 66;
 }
+
 // Test codesnippet cast
 Var_1_code = (int)Var_2_code;
+
 // Variable with record type
 Var_3_code.f1_default = 88;
-
 
 /**
  * ST Example
@@ -23,11 +25,12 @@ IF (Function_1_code(Var_2_code,1)) THEN
 ELSE 
     Var_1_code := 66;
 END_IF;
+
 // Test codesnippet cast
 Var_1_code := #warning "Cast is not supported!";
+
 // Variable with record type
 Var_3_code.f1_default := 88;
-
 
 /**
  * C with own NameProvider Example
@@ -38,10 +41,77 @@ if(TestSystem_Function_1_code(TestSystem_Var_2_code,1)){
 }else{
     TestSystem_Var_1_code = 66;
 }
+
 // Test codesnippet cast
 TestSystem_Var_1_code = (int)TestSystem_Var_2_code;
+
 // Variable with record type
 TestSystem_Var_3_code.f1_default = 88;
 
 
-/* Actifsource ID=[a12233b6-98e4-11ea-8ef1-cb1cd2be9da3,49da7b0b-98e1-11ea-8ef1-cb1cd2be9da3,h/hdxdtWFr//BP8tW+XMLv19ObE=] */
+// STMinus
+
+/**
+ * C Example
+ */ 
+// Test function call
+Function_2_code();
+Function_1_code(Var_1_code,1);
+
+// Test variable assignement
+Var_2_code = Var_3_code;
+Var_3_code = "string";
+
+// Test for
+for(Var_1_code = 1;Var_1_code <= 9;Var_1_code += 1){
+    if(Var_1_code != 0){
+        Var_2_code = Var_3_code; 
+    }else{
+        Var_3_code = "string";
+    }
+}
+
+/**
+ * ST Example
+ */ 
+// Test function call
+Function_2_code();
+Function_1_code(Var_1_code,1);
+
+// Test variable assignement
+Var_2_code := Var_3_code;
+Var_3_code := "string";
+
+// Test for
+Var_1_code := 1;
+WHILE (Var_1_code <= 9) DO
+    IF (Var_1_code <> 0) THEN 
+        Var_2_code := Var_3_code; 
+    ELSE 
+        Var_3_code := "string";
+    END_IF;
+    Var_1_code := Var_1_code + 1;
+END_WHILE;
+
+/**
+ * C with own NameProvider Example
+ */ 
+// Test function call
+TestSystem_Function_2_code();
+TestSystem_Function_1_code(TestSystem_Var_1_code,1);
+
+// Test variable assignement
+TestSystem_Var_2_code = TestSystem_Var_3_code;
+TestSystem_Var_3_code = "string";
+
+// Test for
+for(TestSystem_Var_1_code = 1;TestSystem_Var_1_code <= 9;TestSystem_Var_1_code += 1){
+    if(TestSystem_Var_1_code != 0){
+        TestSystem_Var_2_code = TestSystem_Var_3_code; 
+    }else{
+        TestSystem_Var_3_code = "string";
+    }
+}
+
+
+/* Actifsource ID=[a12233b6-98e4-11ea-8ef1-cb1cd2be9da3,49da7b0b-98e1-11ea-8ef1-cb1cd2be9da3,ObzPrAZKksElH0uwu/I2hIriYyY=] */
