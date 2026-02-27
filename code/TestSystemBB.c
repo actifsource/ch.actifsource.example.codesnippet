7e36675bc3c77b6003695eb223fbb8bf87778c4a
ruti <grischa.rutishauser@actifsource.com>
2026-02-27 15:27:51 +0100

    div3

--- code/TestSystemBB.c
+++ code/TestSystemBB.c
@@ -7,7 +7,7 @@
 if(Function_1_code(Var_2_code,1)){
     Var_1_code = 77; 
 }else{
-    Var_1_code = 66;
+    Var_1_code = 88;
 }
 
 // Test codesnippet cast
@@ -23,7 +23,7 @@ Var_3_code.f1_default = 88;
 IF Function_1_code(Var_2_code,1) THEN 
     Var_1_code := 77; 
 ELSE 
-    Var_1_code := 66;
+    Var_1_code := 88;
 END_IF;
 
 // Test codesnippet cast
@@ -36,17 +36,17 @@ Var_3_code.f1_default := 88;
  * C with own NameProvider Example
  */ 
 // Test codesnippet function variable
-if(TestSystem_Function_1_code(TestSystem_Var_2_code,1)){
-    TestSystem_Var_1_code = 77; 
+if(TestSystemBB_Function_1_code(TestSystemBB_Var_2_code,1)){
+    TestSystemBB_Var_1_code = 77; 
 }else{
-    TestSystem_Var_1_code = 66;
+    TestSystemBB_Var_1_code = 88;
 }
 
 // Test codesnippet cast
-TestSystem_Var_1_code = (int)TestSystem_Var_2_code;
+TestSystemBB_Var_1_code = (int)TestSystemBB_Var_2_code;
 
 // Variable with record type
-TestSystem_Var_3_code.f1_default = 88;
+TestSystemBB_Var_3_code.f1_default = 88;
 
 
 // STMinus
@@ -105,26 +105,26 @@ END_WHILE;
  * C with own NameProvider Example
  */ 
 // Test function call
-TestSystem_Function_2_code();
-TestSystem_Function_1_code(TestSystem_Var_1_code,1);
+TestSystemBB_Function_2_code();
+TestSystemBB_Function_1_code(TestSystemBB_Var_1_code,1);
 
 // Test variable assignement
-TestSystem_Var_2_code = TestSystem_Var_3_code;
-TestSystem_Var_3_code = "string";
-TestSystem_Var_1_code = (1 - (8 / 3)) * 100;
+TestSystemBB_Var_2_code = TestSystemBB_Var_3_code;
+TestSystemBB_Var_3_code = "string";
+TestSystemBB_Var_1_code = (1 - (8 / 3)) * 100;
 
 /*
  * $4b0e401f-98e1-11ea-8ef1-cb1cd2be9da3$ comment
  */
 
 // Test for
-for(TestSystem_Var_1_code = 1;TestSystem_Var_1_code <= 9;TestSystem_Var_1_code += 1){
-    if(TestSystem_Var_1_code != 0){
-        TestSystem_Var_2_code = TestSystem_Var_3_code; 
+for(TestSystemBB_Var_1_code = 1;TestSystemBB_Var_1_code <= 9;TestSystemBB_Var_1_code += 1){
+    if(TestSystemBB_Var_1_code != 0){
+        TestSystemBB_Var_2_code = TestSystemBB_Var_3_code; 
     }else{
-        TestSystem_Var_3_code = "string";
+        TestSystemBB_Var_3_code = "string";
     }
 }
 
 
-/* Actifsource ID=[a12233b6-98e4-11ea-8ef1-cb1cd2be9da3,49da7b0b-98e1-11ea-8ef1-cb1cd2be9da3,IjVGz0Z3Hm9lFtr0OrmjdBk4cjM=] */
+/* Actifsource ID=[a12233b6-98e4-11ea-8ef1-cb1cd2be9da3,49da7b0b-98e1-11ea-8ef1-cb1cd2be9da3,N745+1SLQ1y04arXAYVO6BhEnQ0=] */
